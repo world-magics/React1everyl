717f069dd67ff6f9b871ff344d0de0a0e3035d66
xray-ray <sherzodyarashev1997@gmail.com>
2022-06-08 03:01:02 +0500

    sorted by object

--- src/App.js
+++ src/App.js
@@ -9,17 +9,18 @@ import TableList from './Components/TableList';
 import ToggleBtn from './Components/ToggleBtn';
 import MyButton from './Components/UI/button/MyButton';
 import MyInput from './Components/UI/input/MyInput';
+import MySelect from './Components/UI/select/MySelect';
 function App() {
   
   const [informs,setInforms]=useState([
-  {id:1, firstname:'Ali',surname:"valiyev",jobs:"Data Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
-  {id:2, firstname:'Ali',surname:"valiyev",jobs:"Data Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
-  {id:3, firstname:'Ali',surname:"valiyev",jobs:"Data Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
-  {id:4, firstname:'Ali',surname:"valiyev",jobs:"Data Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
-  {id:5, firstname:'Ali',surname:"valiyev",jobs:"Data Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
-  {id:6, firstname:'Ali',surname:"valiyev",jobs:"Data Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
-  {id:7, firstname:'Ali',surname:"valiyev",jobs:"Data Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
-  {id:8, firstname:'Ali',surname:"valiyev",jobs:"Data Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"} 
+  {id:1, firstname:'dwAli',surname:"valiyev",jobs:"hData Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
+  {id:2, firstname:'vfAli',surname:"valiyev",jobs:"fData Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
+  {id:3, firstname:'vsAli',surname:"valiyev",jobs:"jData Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
+  {id:4, firstname:'rAli',surname:"valiyev",jobs:"vData Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
+  {id:5, firstname:'dAli',surname:"valiyev",jobs:"bData Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
+  {id:6, firstname:'gAli',surname:"valiyev",jobs:"nData Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
+  {id:7, firstname:'bAli',surname:"valiyev",jobs:"aData Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"},
+  {id:8, firstname:'hAli',surname:"valiyev",jobs:"Data Analayzer",years:27,address:"Uzbekistan, Tashkent city Amir Temur Street 104 home",emailAddress:"rayaSoft12OfficeConnect@gmail.com"} 
   ])
   // const [firstname,setName]=useState("");
   // const [surname,setSurname]=useState("");
@@ -35,6 +36,7 @@ function App() {
   const removeInform=(inform)=>{
       setInforms(informs.filter(s=>s.id!==inform.id))
   }
+  const [select,setSelect]=useState('');
 
   // const addPost=(e)=>{
   //   e.preventDefault();
@@ -55,11 +57,31 @@ function App() {
     // setEmailAddress("");
     // console.log(name);
   // }
+  const sortSelect=(sort)=>{
+    setSelect(sort);
+    // console.log(sort);
+    setInforms([...informs].sort((a,b)=>a[sort].localeCompare(b[sort])))
+  }
   return (
     <>
-    <div className="app px-3 my-5">
+    <div className="app px-3 my-5 ">
       <InformForm createInform={createInform}/>
+      <div className='d-flex flex-row-reverse my-3 text-right'>
+         <MySelect 
+         value={select}
+         onChange={sortSelect}
+         defaultValues="Sorted by"
+         options={[
+           {value:"firstname",name:'Firstnames'},
+           {value:"jobs",name:'Jobs'}
+         ]}
+         />
+      </div>
+      {informs.length?
       <TableList remove={removeInform} informse={informs} title={"Uzbekistan, Tashkent Shop Managment System Info"}/>
+      :
+      <h2 className='text-center my-5 text-danger'>Worker inform don't found!.</h2>
+      }
     </div>
   
     </>
---
+++ src/Components/UI/select/MySelect.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MySelect = ({options,defaultValues,onChange,value}) => {
+  return (
+
+      <select 
+      className='form-control my-3'
+    value={value}
+    onChange={e=>onChange(e.target.value)}
+    >
+            <option disabled value="">{defaultValues}</option>
+            {options.map(opt=>( 
+                <option key={opt.value} value={opt.value}>
+                    {opt.name}
+                </option>
+            ))}
+    </select>
+  )
+}
+
+export default MySelect
