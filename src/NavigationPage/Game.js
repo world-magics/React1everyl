b94f26cd829fb3da48b7914b75dda29bf68d7039
unknown <sherzodyarashev1997@gmail.com>
2022-06-28 01:54:24 +0500

    new

---
+++ src/NavigationPage/Game.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Game(props){
+  return (
+    
+        <h2>Game page for interface</h2>
+    
+  )
+}
+
+export default Game
--- src/hooks/useCreateInform.js
+++ src/hooks/useCreateInform.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import { useMemo } from 'react'
 
 export const useSortInforms = (informs,sort) => {
     const  sortedInform=useMemo(()=>{
